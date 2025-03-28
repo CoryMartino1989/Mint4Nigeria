ef166d5629b47caa1871cb0d3c0d788b3efc7659
CoryMartino1989 <77251318+CoryMartino1989@users.noreply.github.com>
2025-03-27 20:52:12 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThirdwebProvider } from "thirdweb/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { defineChain, createThirdwebClient } from "thirdweb";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+const client = createThirdwebClient({
+  clientId: "9db4f27b3ff418eb08e209f9d863cce7",
+});
+const chain = defineChain(1329);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider client={client} chain={chain}>
+        <App />
+      </ThirdwebProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
